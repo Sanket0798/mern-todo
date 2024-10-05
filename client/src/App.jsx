4a239742a5b4fb031b57d4d2ff04bc1b630c71f7
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-05 18:29:10 +0530

    Initial Commit of Files & Folders in Frontend Structure.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
+import Home from "./components/Home";
+import Create from "./components/Create";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Home />
+      <Create />
+    </div>
+  );
 };
 
 export default App;
