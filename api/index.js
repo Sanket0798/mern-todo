fb88c3900abdd899c0568bf61881bbe14a718c61
Sanket0798 <rathodsanket2022@gmail.com>
2024-10-05 18:06:10 +0530

    First Commit

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(8800, () => {
+  console.log("Connected to Backend!");
+});
